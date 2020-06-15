21c64bf3e5ecb7eacb57e288e708a996a4c5bc1d
Alberto De Agostini <albertodeagostini88@gmail.com>
2020-06-15 21:54:16 +0200

    add ga to docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    title: "VirtualizedList documentation"
+    title: "VirtualizedList documentation",
+    plugins: [
+        '@vuepress/last-updated',
+        [
+          '@vuepress/google-analytics',
+          {
+            'ga': 'UA-169425110-1'
+          }
+        ]
+      ],
 }
